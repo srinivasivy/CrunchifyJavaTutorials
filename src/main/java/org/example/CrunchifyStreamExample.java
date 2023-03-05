78a0be87f938afe852ec695ea9d7f22de354c7db
srinivasivy <srinivas_ivy@yahoo.com>
2023-03-05 16:52:36 +0530

    Initial commit.

---
+++ src/main/java/org/example/CrunchifyStreamExample.java
@@ -0,0 +1,6 @@
+package crunchify.com.tutorials;
+public class CrunchifyStreamExample {
+    public static void main(String args[]){
+        System.out.println("Hey.. Thanks for visiting Crunchify.com. We are now pushing file to github from IntelliJ IDEA.");
+    }
+}
